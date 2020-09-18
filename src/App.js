4bd7a0924d45fa2dc1f81f635a1b56a006079e95
donBlak <57069893+donBlak@users.noreply.github.com>
2020-09-18 21:48:17 +0530

    Add Simple Form

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Sample Form</h1>
+      <form>
+        <input type="text" placeholder="Enter your name" />
+        <br />
+        <br />
+        <input type="password" placeholder="Enter password" />
+        <br />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
